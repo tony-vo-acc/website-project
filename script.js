5ef77fbfcb33bb15080476a1e6bf08fe0f396b76
tony-vo-acc <tvo080109@gmail.com>
2023-12-21 21:15:37 -0600

    finishing touches

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ var clicked = 0;
 
 function goOut() {
     var txt = document.getElementById('text');
+    var gif = document.getElementById('gif');
     if (clicked == 0) {
         txt.textContent = "are you sure?"
     } else if (clicked == 1) {
         txt.innerHTML = "are you <i><b>sure</b></i> sure?"
+    } else if (clicked == 2) {
+        txt.innerHTML = "i love you"
+        gif.src = 'https://github.com/OlieWu/tinawina/blob/main/style/images/cute-cat.gif?raw=true'
     }
 
     clicked += 1
